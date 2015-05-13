baa28001775257f0f82be814e81491dc2ebb7555
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2015-05-12 23:20:10 -0400

    Turn script for scraping open_user_js into class

--- open_user_js_scraper.rb
+++ open_user_js_scraper.rb
@@ -3,33 +3,64 @@ require 'open-uri'
 
 require './db_connector'
 
-65.times.each do |page_number|
-  offset_page_number = page_number + 1
+class OpenUserJsScraper
 
-  begin
-    starting_url = "https://openuserjs.org/?p=#{offset_page_number}"
-    starting_page = Nokogiri::XML(open(starting_url))
+  MAX_PAGES = 65
 
-    script_links = starting_page.css('.col-sm-8 .tr-link a.tr-link-a').map { |script| 'https://openuserjs.org' + script.attr('href') + '/source' }
-  rescue Exception => e
-    puts e
-    puts "skipping page #{offset_page_number}"
-    next
+  def scrape
+    (1..MAX_PAGES).each do |page_number|
+      links_to_script_sources(page_number).each do |script_link|
+        script_source = parse_script_source(script_link)
+
+        puts "persisting #{script_link}"
+        script = Script.create(source: 'open_user_js', link: script_link )
+
+        includes = parse_inclusions(script_source)
+        includes.each do |inclusion|
+          Inclusion.create(pattern: inclusion, script_id: script.id)
+        end
+
+        excludes = parse_exclusions(script_source)
+        excludes.each do |exclusion|
+          Exclusion.create(pattern: exclusion, script_id: script.id)
+        end
+      end
+    end
   end
 
-  puts "processing page #{offset_page_number}"
-  script_links.each do |script_link|
+  private
+
+  def parse_script_source(script_link)
     begin
-      code = Nokogiri::XML(open(script_link)).css('#editor').text
-      includes = code.lines.select { |line| line =~ /\/\/ @include/ }.map(&:split).map(&:last).map {|include| include.gsub('%', '\%').gsub('*', '%') }
-      excludes = code.lines.select { |line| line =~ /\/\/ @exclude/ }.map(&:split).map(&:last).map {|exclude| exclude.gsub('%', '\%').gsub('*', '%') }
+      Nokogiri::XML(open(script_link)).css('#editor').text
     rescue Exception => e
       puts e
       puts "skipping script #{script_link}"
-      next
+      ''
     end
-
-    puts "persisting #{script_link}"
-    Script.create(includes: includes, excludes: excludes, source: 'open_user_js', link: script_link )
   end
+
+  def parse_exclusions(code)
+    code.lines.select { |line| line =~ /\/\/ @exclude/ }.map(&:split).map(&:last).map { |exclude| exclude.gsub('%', '\%').gsub('*', '%') }
+  end
+
+  def parse_inclusions(code)
+    code.lines.select { |line| line =~ /\/\/ @include/ }.map(&:split).map(&:last).map { |include| include.gsub('%', '\%').gsub('*', '%') }
+  end
+
+  def links_to_script_sources(page_number)
+    puts "processing page #{page_number}"
+    begin
+      starting_url = "https://openuserjs.org/?p=#{page_number}"
+      starting_page = Nokogiri::XML(open(starting_url))
+
+      starting_page.css('.col-sm-8 .tr-link a.tr-link-a').map { |script| 'https://openuserjs.org' + script.attr('href') + '/source' }
+    rescue Exception => e
+      puts e
+      puts "skipping page #{page_number}"
+      []
+    end
+  end
+
 end
+
